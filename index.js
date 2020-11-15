2b3dea1adf259d4f93973d9ee25c5ff02e72db41
Ruaney <ruan.ramos@hotmail.com>
2020-11-15 00:34:07 -0300

    changes

--- index.js
+++ index.js
@@ -7,6 +7,12 @@ app.use(bodyParser.urlencoded({ extended: true}))
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
+const mongoose = require('mongoose');
+const connectURL =process.env.MONGOODB_URI
+mongoose.connect(connectURL,{useUnifiedTopology: true}, { useNewUrlParser: true } )
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function(){})
 //conecta ao banco
 
 app.use('/api', api)
@@ -14,6 +20,8 @@ app.use(function(err,req,res,next){
     console.log(err);
     res.status(422).send({error: err.message});
 })
+
+
 
 app.get('/', function(req,res){
     res.send('<h2><a href="https://github.com/Ruaney/Freios-Supremos">Mais informações: Git Hub</a></h2>')
